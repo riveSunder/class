1c3fb83709c2bb271558d0c0c54758148996a4a1
Your Name <38316715+riveSunder@users.noreply.github.com>
2025-08-26 17:01:07 +0900

    add damping ratio calculations for reservoir, wip

--- rsvr/plot_damping.py
+++ rsvr/plot_damping.py
@@ -3,12 +3,41 @@ import argparse
 
 import numpy as np
 import torch
+import scipy
+
 
 import matplotlib.pyplot as plt
 import matplotlib
 import matplotlib.animation
 
 from rsvr.simple_reservoir import Reservoir, calc_spectral_radius
+
+def calculate_damping(transient: torch.tensor):
+
+  fit_fn = lambda x, a,b,c, gamma, delay: (a*np.exp(-b*x)+c)*np.sin(x*gamma-delay)
+
+  results = []
+  results_csv = "node_index, lambda, gamma, zeta\n" 
+
+  for node in range(transient.shape[-1]):
+    
+    try:
+      popt, pcov = scipy.optimize.curve_fit(fit_fn, np.arange(transient.shape[0]), transient[:,node], maxfev=120000)
+    except:
+      print(f"curve fitting failed for node {node}")
+      results.append({"node": node, "lambda": "failed", "gamma": "failed", "zeta": "failed"})
+
+    print(f"scipy curve_fit suggests {popt} for neural node {node}")
+    zeta_approx = popt[1]/popt[4]
+    zeta = popt[1] / np.sqrt(popt[1]**2+popt[4]**2)
+
+    print(f"scipy curve_fit suggests lambda={popt[1]}, freq. gamma={popt[4]}, \n"\
+        f"\tdamping ratio ~{zeta_approx:.4e} = {zeta:.4e}")
+    results_csv += f"{node}, {popt[1]:.6f}, {popt[4]:.6f}, {zeta:.6f}\n"
+
+    results.append({"node": node, "lambda": popt[1], "gamma": popt[4], "zeta": zeta})
+
+  return results, results_csv
 
 def calculate_transient(x: torch.tensor, weights: torch.tensor, activation: callable,
     max_steps: int=1024) -> torch.tensor:
@@ -88,6 +117,9 @@ if __name__ == "__main__":
   parser.add_argument("-o", "--output_plot", type=str, default=None, 
       help="filename tag to save output figures (surface gif and side plot of transients)"\
           "leave blank/default to not save output figures")
+  parser.add_argument("-d", "--damping_tag", type=str, default="default",
+      help="use this arg for saving damping info as csv")
+  parser.add_argument("-c", "--calc_exp_fit", type=int, default=0)
 
   args = parser.parse_args()
 
@@ -95,31 +127,40 @@ if __name__ == "__main__":
 
   reservoir = Reservoir(**reservoir_params) 
   in_dim = reservoir.weights_in.shape[0]
-
+    
   # 'impulse' input 
   in_x = torch.ones(1,in_dim)
   x = torch.ones(*torch.matmul(in_x, reservoir.weights_in).shape)
 
   transient = calculate_transient(x, reservoir.weights_hidden.clone(),
       reservoir.act, max_steps=args.transient_steps)
+
   spectral_radius = calc_spectral_radius(reservoir)
-  ax_title = f"Reservoir transient, "\
-      f"spectral radius={spectral_radius:.4f}"
-  sup_title = f"{os.path.split(args.reservoir_filepath)[-2]}" 
 
-  fig_side_filepath = os.path.join("results", f"side_{args.output_plot}.png")
-  animation_filepath = os.path.join("results", f"surf_{args.output_plot}.mp4")
-  gif_filepath = os.path.join("results", f"surf_{args.output_plot}.gif")
+  if args.calc_exp_fit:
+    results_a, results_csv = calculate_damping(transient)
 
-  fig_side, ax_side = plot_side(transient, sup_title=sup_title, ax_title=ax_title)
-  fig, ax = plot_surf(transient, sup_title=sup_title, ax_title=ax_title)
-  
-  num_frames = 512
-  frame_interval = 33
+    csv_filepath = os.path.join("results", f"damping_{args.damping_tag}.csv")
+    with open(csv_filepath, "w") as f:
+      f.write(results_csv)
+  else:
+    ax_title = f"Reservoir transient, "\
+        f"spectral radius={spectral_radius:.4f}"
+    sup_title = f"{os.path.split(args.reservoir_filepath)[-2]}" 
 
-  fig_side.savefig(fig_side_filepath)
-  matplotlib.animation.FuncAnimation(fig, update_surf, 
-      frames=num_frames, interval=frame_interval).save(animation_filepath)
+    fig_side_filepath = os.path.join("results", f"side_{args.output_plot}.png")
+    animation_filepath = os.path.join("results", f"surf_{args.output_plot}.mp4")
+    gif_filepath = os.path.join("results", f"surf_{args.output_plot}.gif")
 
-  matplotlib.animation.FuncAnimation(fig, update_surf, 
-      frames=num_frames, interval=frame_interval).save(gif_filepath)
+    fig_side, ax_side = plot_side(transient, sup_title=sup_title, ax_title=ax_title)
+    fig, ax = plot_surf(transient, sup_title=sup_title, ax_title=ax_title)
+    
+    num_frames = 512
+    frame_interval = 33
+
+    fig_side.savefig(fig_side_filepath)
+    matplotlib.animation.FuncAnimation(fig, update_surf, 
+        frames=num_frames, interval=frame_interval).save(animation_filepath)
+
+    matplotlib.animation.FuncAnimation(fig, update_surf, 
+        frames=num_frames, interval=frame_interval).save(gif_filepath)
